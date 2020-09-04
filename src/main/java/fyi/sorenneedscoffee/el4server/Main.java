80640134df8a37362f9c48919f2dcf0619635665
Joe Sorensen <joseph.md.sorensen@icloud.com>
2020-09-04 15:51:39 -0600

    Initial commit

---
+++ src/main/java/fyi/sorenneedscoffee/el4server/Main.java
@@ -0,0 +1,12 @@
+package fyi.sorenneedscoffee.el4server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private final Logger logger = LoggerFactory.getLogger("Elemental 4");
+
+    public static void main(String[] args) {
+
+    }
+}
